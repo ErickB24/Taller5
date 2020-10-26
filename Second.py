73b9bc19ee678c68bf6490da829ceb7107277c01
ErickB24 <62604202+ErickB24@users.noreply.github.com>
2020-10-25 20:59:17 -0500

    Se cargaron archivos Python del taller

---
+++ Second.py
@@ -0,0 +1,65 @@
+import numpy as np
+import cv2
+import json
+from Clases import projective_camera
+from Clases import set_rotation
+from Clases import projective_camera_project
+import os
+
+path = "/Users/Erick/Desktop/OCTAVO SEMESTRE/PROC DE IMAGENES/Camara y Calibracion/Tablero"  # Ubicacion archivo
+file_name = 'calibration.json'                  # Nombre del JSON para camara celular
+# file_name = 'calibrationweb.json'                  # Nombre del JSON para camara Web
+json_file = os.path.join(path, file_name)       # Abrir archivo
+with open(json_file) as fp:
+    json_data = json.load(fp)                   # leer archivo
+
+# Importar del archivo JSON
+K = json_data['K']                      # Importar K
+d = json_data['d'][0]                   # Importar d
+tilt = json_data['tilt'][0]             # Importar tilt
+pan = json_data['pan'][0]               # Importar pan
+h = json_data['h'][0]                   # Importar h
+
+
+# Tamaño de la imagen
+width = 1920                            # Ancho de la imagen
+height = 1080                           # Alto de la imagen
+
+
+# Parametros Extrinsecos
+R = set_rotation(tilt, pan, 0)          # Matriz de Rotacion R
+t = np.array([0, -d, h])                # Matriz de traslacion t
+
+# Crear Modelo de Camara a partir de la calibracion
+camera = projective_camera(K, width, height, R, t)
+
+
+# Puntos para el Cubo 3D
+square_3D = np.array([[0.5, -0.5,0.5], [0.5, -0.5, -0.5], [-0.5, -0.5, -0.5], [-0.5, -0.5, 0.5]])
+square_2D = projective_camera_project(square_3D, camera)        # Crear Proyeccion
+square_3D_2 = np.array([[0.5, 0.5, 0.5], [0.5, 0.5, -0.5], [-0.5, 0.5, -0.5], [-0.5, 0.5, 0.5]])
+square_2D_2 = projective_camera_project(square_3D_2, camera)    # Crear Proyeccion
+
+# Crear imagen sobre la que se va a escribir
+image_projective = 255 * np.ones(shape=[camera.height, camera.width, 3], dtype=np.uint8)
+
+# lineas para unir cuadrados
+cv2.line(image_projective, (square_2D[3][0], square_2D[3][1]), (square_2D_2[3][0], square_2D_2[3][1]), (255, 255, 0), 3)
+cv2.line(image_projective, (square_2D[0][0], square_2D[0][1]), (square_2D_2[0][0], square_2D_2[0][1]), (255, 255, 0), 3)
+cv2.line(image_projective, (square_2D[1][0], square_2D[1][1]), (square_2D_2[1][0], square_2D_2[1][1]), (255, 255, 0), 3)
+
+# Crear lineas cuadrado 2 2D
+cv2.line(image_projective, (square_2D_2[0][0], square_2D_2[0][1]), (square_2D_2[1][0], square_2D_2[1][1]), (0, 255, 0), 3)
+cv2.line(image_projective, (square_2D_2[1][0], square_2D_2[1][1]), (square_2D_2[2][0], square_2D_2[2][1]), (0, 255, 0), 3)
+cv2.line(image_projective, (square_2D_2[2][0], square_2D_2[2][1]), (square_2D_2[3][0], square_2D_2[3][1]), (0, 255, 0), 3)
+cv2.line(image_projective, (square_2D_2[3][0], square_2D_2[3][1]), (square_2D_2[0][0], square_2D_2[0][1]), (0, 255, 0), 3)
+
+# Crear linea cuadrado 1
+cv2.line(image_projective, (square_2D[0][0], square_2D[0][1]), (square_2D[1][0], square_2D[1][1]), (200, 0, 0), 3)
+cv2.line(image_projective, (square_2D[1][0], square_2D[1][1]), (square_2D[2][0], square_2D[2][1]), (200, 1, 0), 3)
+cv2.line(image_projective, (square_2D[2][0], square_2D[2][1]), (square_2D[3][0], square_2D[3][1]), (200, 1, 0), 3)
+cv2.line(image_projective, (square_2D[3][0], square_2D[3][1]), (square_2D[0][0], square_2D[0][1]), (200, 1, 0), 3)
+cv2.line(image_projective, (square_2D[2][0], square_2D[2][1]), (square_2D_2[2][0], square_2D_2[2][1]), (255, 255, 0), 3)
+
+cv2.imshow("Image", image_projective)       # Mostrar cubo
+cv2.waitKey(0)
---
+++ main.py
@@ -0,0 +1,81 @@
+import numpy as np
+import cv2
+import glob
+import os
+import json
+
+# Criterio de iteraciones
+criterio = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# Preparar puntos
+objp = np.zeros((7 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:7].T.reshape(-1, 2)
+
+# Arreglos para guardar los puntos del objeto
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+
+# Ubicacion de las imagenes (tablero de ajedrez)
+path = "/Users/Erick/Desktop/OCTAVO SEMESTRE/PROC DE IMAGENES/Camara y Calibracion/Tablero"
+
+# Camara Celular
+path_file = os.path.join(path, 'IGM*.PNG')  # Elegir archivos que contengan IGM
+# Camara Web
+# path_file = os.path.join(path, 'WIN*.jpg')  # Elegir archivos que contengan WIN (camara de Windows)
+
+images = glob.glob(path_file)       # Glob de Imagenes
+
+for fname in images:                                                # Para cada una de las imagenes :
+
+    img = cv2.imread(fname)                                         # Leer Imagen
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                # Imagen en Grises
+
+    # Encontrar las esquinas del tablero de Ajedrez
+    ret, corners = cv2.findChessboardCorners(img_gray, (7, 7), None)
+
+    # Si se encuentran las imagenes:
+    if ret == True:
+        objpoints.append(objp)
+
+        # encontrar pixel con precision sub-pixel
+        corners2 = cv2.cornerSubPix(img_gray, corners, (11, 11), (-1, -1), criterio)
+        imgpoints.append(corners2)  # Acumular
+
+        # Dibujar esquinas sobre la imagen original
+        img = cv2.drawChessboardCorners(img, (7, 7), corners2, ret)
+        cv2.imshow('img', img)  # mostrar la imagen
+        cv2.waitKey(400)        # Esperar 400 ms
+
+cv2.destroyAllWindows()
+
+#Calibrar camara
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_gray.shape[::-1], None, None)
+
+print(mtx)
+print(dist)
+
+file_name = 'calibration.json'                  # nombre del archivo de calibracion
+# file_name = 'calibrationweb.json'               # nombre del archivo de calibracion camara web
+json_file = os.path.join(path, file_name)
+d = [3.0]
+tilt = [30.0]
+pan = [0]
+h = [2.0]
+
+# convertir a lista para exportar a JSON
+data = {
+
+    'K': mtx.tolist(),              # Matriz K
+    'distortion': dist.tolist(),    # Vector distorcion
+    'd' : d,                        # Distancia
+    'tilt' : tilt,                  # Angulo Tilt
+    'pan' : pan,                    # Angulo Pan
+    'h' : h                         # altura H
+}
+
+# Exportar a JSON
+with open(json_file, 'w') as fp:
+    json.dump(data, fp, sort_keys=True, indent=1, ensure_ascii=False)
+
+
+
